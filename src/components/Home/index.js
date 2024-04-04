b45c0f245dc452cdbf859c4a738df055010f49b7
Anthony Tran <anthony.tran99@hotmail.com>
2024-04-04 15:01:39 +1100

    Test commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,8 +3,8 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Welcome to the Homepage!</h1>
-      <p>This is a boilerplate code for a React homepage.</p>
+      <h1>SOIL</h1>
+      <p>Test commit</p>
     </div>
   );
 }
