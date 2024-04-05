edf8f7b486d1b663bd213dfbe99a927b6bde9727
rhrmit <s3436258@student.rmit.edu.au>
2024-04-05 13:27:44 +1100

    ron/page-layout: changes Home page name to 'Home.js'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
