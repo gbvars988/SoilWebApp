f6cf910c94eeac9328d46e1623b7da37aec332c9
Anthony Tran <anthony.tran99@hotmail.com>
2024-04-05 18:32:31 +1100

    test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
+        <Route index element={<Home />} />)
+        <Route path="home" element={<Home />} />)
+        <Route path="about" element={<About />} />)
+        <Route path="deals" element={<Deals />} />)
+        <Route path="contact" element={<Contact />} />)
+        <Route path="login" element={<Login />} />)
+        <Route path="signup" element={<Signup />} />)
       </Routes>
     </BrowserRouter>
   );
