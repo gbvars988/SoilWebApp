e74a0411847481029c430695604287e6684a039f
Anthony Tran <anthony.tran99@hotmail.com>
2024-04-08 11:39:23 +1000

    branch test

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -6,6 +6,14 @@ function Login() {
     <div>
       <Navbar />
       <h1> Login </h1>
+      <p>
+        {" "}
+        Signin: The Signin component is also a form with only email and password
+        fields for signing in: After the proper validations (email and password)
+        and checks have been performed, the user will be redirected to the
+        profile page. Provide a visual cue upon successful login. The visual cue
+        could be a text or a pop-up message.
+      </p>
     </div>
   );
 }
