abca3ce84d382915057a3744e3b722f40ff7c246
rhrmit <s3436258@student.rmit.edu.au>
2024-04-15 17:09:52 +1000

    adds modal popup for specials products

--- src/components/Deals/WeeklySpecials.js
+++ src/components/Deals/WeeklySpecials.js
@@ -1,47 +1,70 @@
-import React from 'react';
-import './weeklyspecials.css';
-import Eggs from '../../assets/eggs.jpg';
-import Tomato from '../../assets/tomato.jpg';
-import Zucchini from '../../assets/zucchini.jpg';
-import Pear from '../../assets/pear.jpg';
-import Honeydew from '../../assets/honeydew.jpg';
-import Spinach from '../../assets/spinach.jpg';
-import Mushroom from '../../assets/mushroom.jpg';
-import Potato from '../../assets/potato.jpg';
-import Pumpkin from '../../assets/pumpkin.jpg';
-import Asparagus from '../../assets/asparagus.jpg';
-
+import { React, useState } from "react";
+import "./weeklyspecials.css";
+import Eggs from "../../assets/eggs.jpg";
+import Tomato from "../../assets/tomato.jpg";
+import Zucchini from "../../assets/zucchini.jpg";
+import Pear from "../../assets/pear.jpg";
+import Honeydew from "../../assets/honeydew.jpg";
+import Spinach from "../../assets/spinach.jpg";
+import Mushroom from "../../assets/mushroom.jpg";
+import Potato from "../../assets/potato.jpg";
+import Pumpkin from "../../assets/pumpkin.jpg";
+import Asparagus from "../../assets/asparagus.jpg";
+import Modal from "./Modal";
 
 function WeeklySpecials(props) {
   const imageMap = {
-    'eggs': Eggs,
-    'tomato': Tomato,
-    'zucchini': Zucchini,
-    'pear': Pear,
-    'honeydew': Honeydew,
-    'spinach': Spinach,
-    'mushroom': Mushroom,
-    'potato': Potato,
-    'pumpkin': Pumpkin,
-    'asparagus': Asparagus
+    eggs: Eggs,
+    tomato: Tomato,
+    zucchini: Zucchini,
+    pear: Pear,
+    honeydew: Honeydew,
+    spinach: Spinach,
+    mushroom: Mushroom,
+    potato: Potato,
+    pumpkin: Pumpkin,
+    asparagus: Asparagus,
+  };
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
   };
 
+  const handleCloseModal = () => {
+    setSelectedItem(null);
+  };
 
   return (
     <div className="specials-container-bg">
       <h1>Weekly Specials</h1>
-        <ul className="specials-container">
-          {props.specials.map((item) => (
-            <li className="specials-item" key={item.id}>
-              <h3>{item.product}</h3>
-              <p>{item.price}</p>
-              <img src={imageMap[item.img_name]}></img>
-            </li>
-          ))}
-        </ul>
+      <ul className="specials-container">
+        {props.specials.map((item) => (
+          <li
+            className="specials-item"
+            key={item.id}
+            onClick={() => handleItemClick(item)}
+          >
+            <h3>{item.product}</h3>
+            <p>{item.price}</p>
+            <img src={imageMap[item.img_name]}></img>
+          </li>
+        ))}
+      </ul>
+      {/* {selectedItem !== null && (
+        <Modal isOpen={selectedItem !== null} onClose={handleCloseModal} />
+      )} */}
+      <Modal isOpen={selectedItem !== null} onClose={handleCloseModal}>
+        {selectedItem && (
+          <div>
+            <img src={imageMap[selectedItem.img_name]}></img>
+            <p>{selectedItem.product}</p>
+            <>{selectedItem.info}</>
+          </div>
+        )}
+      </Modal>
     </div>
-
-  )
+  );
 }
 
 export default WeeklySpecials;
--- src/data/specialsrepository.js
+++ src/data/specialsrepository.js
@@ -1,72 +1,81 @@
-import React from 'react';
-
+import React from "react";
 
 function initSpecials() {
   if (localStorage.getItem("specials") !== null) {
-      return
+    return;
   }
 
   const specials = [
     {
-      product: "Free-range eggs", 
+      product: "Free-range eggs",
       price: "$2",
       id: 0,
-      img_name: "eggs"
+      img_name: "eggs",
+      info: "Fresh organic free-range eggs sourced locally",
     },
     {
-      product: "Organic tomato", 
+      product: "Organic tomato",
       price: "$1",
       id: 1,
-      img_name: "tomato"
+      img_name: "tomato",
+      info: "Perfectly ripe and delicious organically grown tomato",
     },
     {
       product: "Asparagus Bunch Organic",
       price: "$5",
       id: 2,
-      img_name: "asparagus"
+      img_name: "asparagus",
+      info: "Organic wild asparagus loose or bunches direct from the farm. Much more tender and sweet than regular asparagus. It can also be enjoyed raw as a quick snack or quickly pan fried with some butter and garlic.",
     },
     {
       product: "Mushrooms Flat Brown 120g",
       price: "$8",
       id: 3,
-      img_name: "mushroom"
+      img_name: "mushroom",
+      info: "Prized for their rich, earthy taste, they’re also a good source of potassium, selenium, protein, and antioxidants, while containing no fat or cholesterol.",
     },
     {
       product: "Potatoes Kipfler Organic",
       price: "$3",
       id: 4,
-      img_name: "potato"
+      img_name: "potato",
+      info: "ACO certified organic & Australian grown. Usually boiled, mashed or roasted. Red skin and lovely creamy flesh.",
     },
     {
       product: "Pumpkin Butternut",
       price: "$5",
       id: 5,
-      img_name: "pumpkin"
+      img_name: "pumpkin",
+      info: "Cut and bagged. Best for roasting.",
     },
     {
       product: "Spinach Bunch Organic",
       price: "$3.50",
       id: 6,
-      img_name: "spinach"
+      img_name: "spinach",
+      info: "uper nutritional english spinach direct from the farm.",
     },
     {
       product: "Zucchini Organic",
       price: "$4",
       id: 7,
-      img_name: "zucchini"
+      img_name: "zucchini",
+      info: "Crunchy and juicy spray free zucchini. Great in a stir fry or as noodles for a gluten free spin on pasta.",
     },
     {
       product: "Honeydew Organic",
       price: "$7",
       id: 8,
-      img_name: "honeydew"
+      img_name: "honeydew",
+      info: "Super sweet and juicy sourced direct from our local farm.",
     },
     {
       product: "Nashi Pear Organic",
       price: "$1.40",
       id: 9,
-      img_name: "pear"
-    }
+      img_name: "pear",
+      info: "New season nashi pears picked Direct from the orchard.",
+    },
   ];
 
   localStorage.setItem("specials", JSON.stringify(specials));
@@ -77,7 +86,4 @@ function getSpecials() {
   return JSON.parse(specials);
 }
 
-export {
-  initSpecials,
-  getSpecials
-}
+export { initSpecials, getSpecials };
